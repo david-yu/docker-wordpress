af5ac740625af9c8b01cc7da593a8a4a0c010455
David Yu <david.yu@docker.com>
2018-04-13 14:34:07 -0700

    moving scripts to swarm folder

--- scripts/create-secrets.sh
+++
@@ -1,5 +0,0 @@
-echo "mysql-root-password-secret" | docker secret create MYSQL_ROOT_PASSWORD -
-echo "mysql-password-secret" | docker secret create MYSQL_PASSWORD -
-echo "wordpress" | docker secret create MYSQL_USER -
-echo "wordpress" | docker secret create MYSQL_DATABASE -
-docker secret ls
